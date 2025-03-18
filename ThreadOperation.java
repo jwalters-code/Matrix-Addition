597bc6a37b7d18a7f51940e751035e0a0cfad41f
jwalters-code <jadedinparadise@gmail.com>
2025-03-18 14:25:38 -0600

    set up instance variables

--- ThreadOperation.java
+++ ThreadOperation.java
@@ -6,6 +6,13 @@ public class ThreadOperation extends Thread {
 	two matrices and a quadrant indicator. The indicator could be a String, an int, 
 	an enum or a set of indexes. It's up to you.*/
 	
+	private int rows;
+	private int columns;
+	private int[][] matrix1 = new int[rows][columns];
+	private int[][] matrix2 = new int[rows][columns];
+	private quadrant = 0;
+	
 	//constructor
+	
 
 }
