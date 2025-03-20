4814de6fa516b4caba4007b10621b040a54411f9
jwalters-code <jadedinparadise@gmail.com>
2025-03-20 16:16:50 -0600

    added an enum for quadrant of threadOperation

--- ThreadOperation.java
+++ ThreadOperation.java
@@ -1,4 +1,14 @@
-
+/*
+Jade Walters
+CSCI 2251
+Assignment 4: Tic Tac Toe
+Purpose: Create a tic tac toe game using classes
+Sources: --I got help with using an enum in a constructor 
+from StackOverflow (I had trouble finding it on a proper 
+tutorial website).
+source: 
+https://stackoverflow.com/questions/32733084/pass-a-simple-enum-into-a-constructor-in-java
+*/
 
 public class ThreadOperation extends Thread {
 	
@@ -8,14 +18,22 @@ public class ThreadOperation extends Thread {
 	
 	private int[][] matrix1;
 	private int[][] matrix2;
-	private int quadrant;
+	private Quadrant quadrant;
+	
+	//https://stackoverflow.com/questions/32733084/pass-a-simple-enum-into-a-constructor-in-java
+	enum Quadrant {
+		ONE,
+		TWO,
+		THREE,
+		FOUR
+	}
 	
 	//default constructor
 	public ThreadOperation() {
 	}
 	
 	//constructor
-	public ThreadOperation(int[][] matrix1, int[][] matrix2, int quadrant) {
+	public ThreadOperation(int[][] matrix1, int[][] matrix2, Quadrant quadrant) {
 		this.matrix1 = matrix1;
 		this.matrix2 = matrix2;
 		this.quadrant = quadrant;
